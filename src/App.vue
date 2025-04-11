6677aef12d0281cf8c942b0ed6af3d42239227ec
Hayden Barogh <hbarogh@gmail.com>
2025-04-10 20:22:36 -0500

    feat: add NavBar component and integrate it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+  <NavBar/>
   <router-view></router-view>
 </template>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+<div class="nav-bar">
+    <div class="nav-buttons-wrap">
+      <router-link to="/Western">
+        <button>Western</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+
+<style>
+
+</style>
