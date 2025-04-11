078fb0c6ab64cf7cb3b6bc85e02e668327218a23
Hayden Barogh <hbarogh@gmail.com>
2025-04-10 21:39:11 -0500

    fix: update NavBar component layout and adjust body styles

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,11 +8,20 @@
       <router-link to="/Western">
         <button>Western</button>
       </router-link>
+
+      <router-link to="/HomePage">
+        <button>Home</button>
+      </router-link>
     </div>
   </div>
 </template>
 
 
 <style>
-
+.nav-bar {
+  display: flex;
+  align-items: center; /* Centers items vertically */
+  justify-content: space-between; 
+  flex-wrap: wrap;
+}
 </style>
