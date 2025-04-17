89c21f5d9c45b02236f2660aa7df46ed189f17e4
Hayden Barogh <hbarogh@gmail.com>
2025-04-16 21:19:23 -0500

    fix: update API_URL to use BACKEND_API_URL environment variable

--- front-end/src/views/PhotoCollectionPage.vue
+++ front-end/src/views/PhotoCollectionPage.vue
@@ -6,7 +6,7 @@ import axios from 'axios';
 const route = useRoute()
 const collection = ref(route.params.collection as string); 
 const photos = ref<string[]>([]);
-const API_URL = import.meta.env.BASE_URL || 'http://localhost:5000';
+const API_URL = import.meta.env.BACKEND_API_URL|| 'http://localhost:5000';
 async function fetchPhotos(collection: string): Promise<void> {
   try{
     const response = await axios.get(`${API_URL}/api/photos/${collection}`);
