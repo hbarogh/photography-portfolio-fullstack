6d84262582dcfcc18fb0d562ea1eff6081ad02bc
Hayden Barogh <hbarogh@gmail.com>
2025-04-30 17:17:50 -0500

    feat: add ScrollToTopButton component and integrate it into App.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
+import ScrollToTopButton from './components/ScrollToTopButton.vue';
 </script>
 
 <template>
   <NavBar/>
+  <ScrollToTopButton/>
   <router-view/>
 </template>
 
---
+++ front-end/src/components/ScrollToTopButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {ref, onMounted, onBeforeUnmount} from 'vue';
+import { NButton, NIcon } from 'naive-ui';
+import {ArrowUpCircleOutline} from '@vicons/ionicons5'; 
+const showButton = ref(false)
+function handleScroll():void {
+  showButton.value = window.scrollY > 250 
+}
+
+function scrollToTop():void {
+  window.scrollTo({ top: 0, behavior: 'smooth'})
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+</script>
+
+<template>
+  <n-button v-show="showButton" @click="scrollToTop" style="position: fixed; bottom: 2rem; right: 2rem; z-index: 1000" circle type="default" >
+    <n-icon size="24">
+      <ArrowUpCircleOutline/>
+    </n-icon>
+  </n-button>
+</template>
+
+
+<style>
+
+</style>
