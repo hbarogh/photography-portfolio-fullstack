954c5f878fc5553f05d4fa3abf0f7910e980f79d
Hayden Barogh <hbarogh@gmail.com>
2025-04-30 22:34:42 -0500

    feat: add @vercel/analytics dependency and integrate it into App.vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
 import ScrollToTopButton from './components/ScrollToTopButton.vue';
+import {Analytics} from '@vercel/analytics/next';
 </script>
 
 <template>
   <NavBar/>
   <ScrollToTopButton/>
   <router-view/>
+  <Analytics/>
 </template>
 
 <style scoped>
