044b9cf868600c4b19d0887cab024f0bfe2da9d9
Hayden Barogh <hbarogh@gmail.com>
2025-04-30 22:35:55 -0500

    fix: update Analytics import to use '@vercel/analytics/vue'

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
 import ScrollToTopButton from './components/ScrollToTopButton.vue';
-import {Analytics} from '@vercel/analytics/next';
+import {Analytics} from '@vercel/analytics/vue';
 </script>
 
 <template>
