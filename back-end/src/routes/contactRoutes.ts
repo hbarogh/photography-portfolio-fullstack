c39f37927531ae7b9dfc13ce9edfc1b381ccd4b0
Hayden Barogh <hbarogh@gmail.com>
2025-05-05 23:34:58 -0500

    added resend to send uses an email after they fill out the contact form

--- back-end/src/routes/contactRoutes.ts
+++ back-end/src/routes/contactRoutes.ts
@@ -1,11 +1,11 @@
 //this file is used for the api for savign the contact form data to the database
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-
+import { Resend } from 'resend';
 
 const router = express.Router();
 const prisma = new PrismaClient();
-
+const resend = new Resend(process.env.EMAIL_API_KEY);
 //here I am making the post api now 
 
 router.post('/', async (req, res) => {
@@ -14,7 +14,22 @@ router.post('/', async (req, res) => {
   try {
     const saved = await prisma.contactMessage.create({
       data: {firstName, lastName, email, phone, subject, message },
-    })
+    });
+
+    resend.emails.send({
+      from: process.env.FROM_EMAIL!,
+      to: email,
+      subject:  `Thank you for your inquiry, ${firstName}!`,
+      html: `
+        <p>Hi ${firstName},</p>
+        <p>Thanks for reaching out about <strong>${subject}</strong>.</p>
+        <p>I'll review your message and get back to you soon.</p>
+        <p>- Hayden</p>
+      `
+    }).catch(err => {
+      console.error('Email send error: ', err);
+    });
+    
     res.status(201).json({ success: true, message: 'Message submitted successfully!', data: saved})
   }
   catch (error){
