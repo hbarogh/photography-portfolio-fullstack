e50dad6d5cd46467c687b25ac914cc8a6438e810
Hayden Barogh <hbarogh@gmail.com>
2025-05-05 19:48:17 -0500

    feat: wrap main components in n-config-provider for theme support

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,13 +2,16 @@
 import NavBar from './components/NavBar.vue';
 import ScrollToTopButton from './components/ScrollToTopButton.vue';
 import {Analytics} from '@vercel/analytics/vue';
+import { lightTheme, NConfigProvider } from 'naive-ui';
 </script>
 
 <template>
-  <NavBar/>
-  <ScrollToTopButton/>
-  <router-view/>
-  <Analytics/>
+  <n-config-provider :theme="lightTheme">
+    <NavBar/>
+    <ScrollToTopButton/>
+    <router-view/>
+    <Analytics/>
+  </n-config-provider>
 </template>
 
 <style scoped>
--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -1,75 +1,80 @@
 <script setup lang="ts">
-  import { NMenu } from 'naive-ui';
-  import { useRouter } from 'vue-router';
-  import {menuOptions} from '../constants/menu-options';
-  import {ref, onMounted, onBeforeUnmount } from 'vue';
-  import { mobileMenuOptions } from '../constants/mobile-menu-options';
-  const router = useRouter()
-  const isMobile = ref(false)
+import { NMenu } from "naive-ui";
+import { useRouter } from "vue-router";
+import { menuOptions } from "../constants/menu-options";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { mobileMenuOptions } from "../constants/mobile-menu-options";
+const router = useRouter();
+const isMobile = ref(false);
 
-  function handleMenuSelect(key: string){
-    if (key === 'Home') {
-      router.push({ name: 'Home' })
-    } 
-    else if (key === 'AboutPage') {
-      router.push({ name: 'AboutPage' })
-    } 
-    else if (key === 'ContactPage'){
-      router.push({name: 'ContactPage'})
-    }
-    else {
-      // Anything else is assumed to be a collection
-      router.push({ name: 'PhotoCollectionPage', params: { collection: key } })
-     }
+function handleMenuSelect(key: string) {
+  if (key === "Home") {
+    router.push({ name: "Home" });
+  } else if (key === "AboutPage") {
+    router.push({ name: "AboutPage" });
+  } else if (key === "ContactPage") {
+    router.push({ name: "ContactPage" });
+  } else {
+    // Anything else is assumed to be a collection
+    router.push({ name: "PhotoCollectionPage", params: { collection: key } });
   }
+}
 
-  function checkSize(): void {
-    isMobile.value = window.innerWidth <= 500
-  }
+function checkSize(): void {
+  isMobile.value = window.innerWidth <= 500;
+}
 
-  onMounted(() => {
-    checkSize()
-    window.addEventListener('IsMobile', checkSize)
-  })
+onMounted(() => {
+  checkSize();
+  window.addEventListener("IsMobile", checkSize);
+});
 
-  onBeforeUnmount(() => {
-    window.removeEventListener('IsMobile', checkSize)
-  })
- 
+onBeforeUnmount(() => {
+  window.removeEventListener("IsMobile", checkSize);
+});
 </script>
 
-
 <template>
-  <div class="nav-links" v-if=!isMobile>
-    <n-menu class="nav-bar"
-    mode="horizontal" 
-    :options="menuOptions"
-    @update:value="handleMenuSelect"
+  <div class="nav-links" v-if="!isMobile">
+    <n-menu
+      class="nav-bar"
+      mode="horizontal"
+      :options="menuOptions"
+      @update:value="handleMenuSelect"
     />
   </div>
   <div class="mobile-menu" v-else>
-    <n-menu class="mobileMenu"
-    mode="vertical"
+    <!-- <n-dropdown class="mobileMenu"
     :options="mobileMenuOptions"
-    @update:value="handleMenuSelect"
+    trigger="click"
+    @select:value="handleMenuSelect"
+    >
+      <n-button>
+        Menu
+      </n-button>
+    </n-dropdown> -->
+    <n-menu
+      class="mobileMenu"
+      :options="mobileMenuOptions"
+      mode="vertical"
+      @update:value="handleMenuSelect"
     />
   </div>
-    
 </template>
-
 
 <style>
 .nav-bar {
   display: flex;
   align-items: center; /* Centers items vertically */
-  justify-content: space-between; 
+  justify-content: space-between;
   flex-wrap: wrap;
 }
 
-.mobile-menu{
+.mobile-menu {
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 0 18px 0 18px;
 }
+
 </style>
