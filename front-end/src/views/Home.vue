ee774db378a9a412d62bb0e38dbc45f8885e88bf
Hayden Barogh <hbarogh@gmail.com>
2025-05-12 20:10:45 -0500

    feat: implement photo fetching and display with skeleton loader on home page

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,10 +1,50 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { NImage, NSkeleton, NCard, NGrid, NGridItem } from 'naive-ui';
+const collection = ref("HomePage");
+const photos = ref<string[]>([]);
+const skeleton = ref(true)
+
+async function fetchPhotos(collection: string): Promise<void> {
+  try{
+    const response = await axios.get(`https://backend-3497.onrender.com/api/photos/${collection}`);
+    photos.value = response.data.map((img: any) => img.optimized_url);
+    console.log(`response: ${response}`);
+  }
+  catch (error){
+    alert(`Axios error: ${error}`);
+    console.error('Axios error:', error);
+  }
+  finally{
+    skeleton.value = false 
+  }
+}
+
+onMounted(() => {
+  fetchPhotos(collection.value)
+});
+
+
 </script>
 
 
 <template>
-<div>
+  <div>
     <h1 class="home-title">Home Page</h1>
+  </div>
+  <n-skeleton v-if="skeleton"  :repeat="2" height="40px" width="60%" :sharp="false"/>
+  <div class="hero-img">
+    <n-image :src="photos[0]" width="100%" object-fit="cover"/>
+    <n-grid s:1 m:2: l: 3 1-3 responsive="screen">
+      <n-grid-item v-for="(url, index) in photos" :key="index">
+        <n-card >
+          <n-image :src=url>
+            
+          </n-image>
+        </n-card>
+      </n-grid-item>
+    </n-grid>
   </div>
 </template>
 
@@ -14,4 +54,11 @@
 .home-title{
   text-align: center;
 }
+
+.hero-img{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 </style>
